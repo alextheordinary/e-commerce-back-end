4df9940040effdec36ca9bee4642f15437eb4b59
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-02 17:26:18 -0400

    fixed syntax error in belongstomany declarations that was adding erroneously named foreign keys. implemented product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,17 +7,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  const productsData = await Product.findAll({
-    include: [Category, 
-      {model: Tag, through: ProductTag}
-]});
-  res.status(200).json(productsData);
+  try {
+    const productsData = await Product.findAll({
+      include: [Category,
+        { model: Tag, through: ProductTag }
+      ]
+    });
+    res.status(200).json(productsData);
+  } catch {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category,
+        { model: Tag, through: ProductTag }
+      ]
+    });
+    res.status(200).json(productData);
+  } catch {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
@@ -94,8 +109,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 module.exports = router;
