4daed55617e071fd54be6cf11979a4e5b78dc9c2
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-02 10:39:00 -0400

    Implemented get / route for product-routes. db seeding is currently failing. need to troubleshoot.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const productsData = await Product.findAll();
+  console.log(productsData);
+  res.status(200).json(productsData);
 });
 
 // get one product
